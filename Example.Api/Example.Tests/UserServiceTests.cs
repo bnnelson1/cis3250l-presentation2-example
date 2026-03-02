73bb0cc2463c757047cc2ad278d0a4b569593b60
Brian Nelson <122384363+bnnelson1@users.noreply.github.com>
2026-03-02 15:47:00 -0500

    Added comments to example test.

--- Example.Api/Example.Tests/UserServiceTests.cs
+++ Example.Api/Example.Tests/UserServiceTests.cs
@@ -11,15 +11,19 @@ public class UserServiceTests
     [Fact]
     public void Requesting_an_existing_user_by_username_returns_the_user()
     {
+        // Arrange
         const string username = "jane_doe";
         var user = new User { Id = 2, Username = username, Email = "janedoe@proton.me" };
+        
         var repository = Substitute.For<IUserRepository>();
         repository.GetUserByUsername(username).Returns(user);
 
+        // Act
         var service = new UserService(repository);
         var returnedUser = service.GetUserByUsername(username);
         
+        // Assert
         returnedUser.ShouldBeEquivalentTo(user);
-        repository.Received().GetUserByUsername(username);
+        repository.Received().GetUserByUsername(username);  // Included here for completion
     }
 }
