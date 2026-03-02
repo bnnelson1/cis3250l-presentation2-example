113e5176bb416afa28bf2ff47a9e55b4881eb7bb
Brian Nelson <122384363+bnnelson1@users.noreply.github.com>
2026-03-02 10:19:12 -0500

    Replaced name properties of User with a Username property.

--- Example.Api/Example.Data/Models/User.cs
+++ Example.Api/Example.Data/Models/User.cs
@@ -3,7 +3,6 @@ namespace Example.Data.Models;
 public class User
 {
     public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
 }
